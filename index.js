3eb0d9e1542d980343213a30d2329289ef61203b
sharon <futureyuanjing@gmail.com>
2018-01-08 01:38:32 -0800

    fix mongo error

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
-
-mongoose.connect(keys.mongoURI, {
-  useMongoClient: true
-});
 
 const app = express();
 
