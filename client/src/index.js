becb6eb27fee2c2d957145056c9112df37608d9c
sharon <futureyuanjing@gmail.com>
2018-01-08 18:24:40 -0800

    add index.css

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,8 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
-
-// console.log('stripe key is:', process.env.REACT_APP_STRIPE_KEY);
-// console.log('environment is:', process.env.NODE_ENV);
