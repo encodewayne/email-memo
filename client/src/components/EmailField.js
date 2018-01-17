78ffd0b92ade68d7083729cc077cd5a70935a9cd
sharon <futureyuanjing@gmail.com>
2018-01-17 06:13:05 -0800

    extract emailField css

--- client/src/components/EmailField.js
+++ client/src/components/EmailField.js
@@ -3,11 +3,9 @@ import React from 'react';
 export default ({ input, label, meta: { error, touched } }) => {
   return (
     <div>
-      <h5>{label}</h5>
-      <input {...input} type="text" style={{ marginBottom: '5px' }} />
-      <div className="red-text" style={{ marginBottom: '20px' }}>
-        {touched && error}
-      </div>
+      <h3>{label}</h3>
+      <input {...input} type="text" id="memo-input" />
+      <div className="red-text memo-input-error">{touched && error}</div>
     </div>
   );
 };
